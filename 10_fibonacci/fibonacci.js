b3bbe6645b6d6b852f48b6d0d52e1ba4e56755e4
jvictormorais <jose.alvarezmorais@gmail.com>
2023-01-25 15:46:10 -0300

    Do exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+if(n<0){
+    return "OOPS"
+}else{
+    const result = [1, 1];
+    for (var i = 2; i < n; i++) {
+        result.push(result[i-2] + result[i-1]);
+    }
+    return result[n-1]; 
+}
 
 };
 
