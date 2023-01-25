f1e1c8508a13f071bdc3c1f9a1ad3f78fd901a8f
jvictormorais <jose.alvarezmorais@gmail.com>
2023-01-25 20:45:26 -0300

    Do exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = { age: 0 };
+    people.forEach(people => {
+        const age = people.yearOfDeath ? people.yearOfDeath - people.yearOfBirth : new Date().getFullYear() - people.yearOfBirth;
+        if (age > oldest.age) {
+            oldest = { ...people, age };
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
