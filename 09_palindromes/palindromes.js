34cb7305d310873021271e8725031eb4040f654f
jvictormorais <jose.alvarezmorais@gmail.com>
2023-01-25 15:18:16 -0300

    Do exercise 09_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/\s/g, ''); //remove space
+    str = str.replace(/[^\w\s]/gi, ''); // remove characters that are not words and space
+    str = str.toLowerCase(); //to lower case
+    let palStr = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+         palStr += str[i];
+    } 
+    if(str == palStr) {
+        return true
+    } return false;
 };
 
 // Do not edit below this line
