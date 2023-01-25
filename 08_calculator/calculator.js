05768802a05fa26c4f001444996342f2ad74036a
jvictormorais <jose.alvarezmorais@gmail.com>
2023-01-25 14:38:50 -0300

    Do exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(ourArray) {
+  let sum = 0;
+  for (let i = 0; i < ourArray.length; i += 1) {
+    sum += ourArray[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length
+      ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+      : 0;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+    var f = [];
+    if (num1 == 0 || num1 == 1)
+      return 1;
+    if (f[num1] > 0)
+      return f[num1];
+    return f[num1] = factorial(num1-1) * num1;
 };
 
 // Do not edit below this line
